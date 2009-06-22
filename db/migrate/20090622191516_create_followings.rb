48ed37ed1e8d5baae7e7537b0bec1768fcccf320
Kyle Maxwell <kyle@kylemaxwell.com>
2009-06-22 12:19:08 -0700

    followings

---
+++ db/migrate/20090622191516_create_followings.rb
@@ -0,0 +1,18 @@
+class CreateFollowings < ActiveRecord::Migration
+  def self.up
+    create_table :followings do |t|
+      t.integer :follower_id
+      t.integer :followee_id
+
+      t.timestamps
+    end
+    add_index :followings, :follower_id
+    add_index :followings, :followee_id
+  end
+
+  def self.down
+    remove_index :followings, :follower_id
+    remove_index :followings, :followee_id
+    drop_table :followings
+  end
+end
