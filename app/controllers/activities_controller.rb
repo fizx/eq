e04ee6d6cd657a8e60d0123d061d41be4b7a8cab
Kyle Maxwell <kyle@kylemaxwell.com>
2009-06-29 14:13:31 -0700

    private activities

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -3,7 +3,7 @@ class ActivitiesController < ApplicationController
     @events = current_user.events
     @interest = current_user.new_interest
     @stream = Interest.of_friends_of(current_user).paginate(:page => params[:page], :order => "interests.id DESC")
-    @activities = Category.find(:all, :conditions => {:type => "Activity", :parent_id => params[:parent]}, :limit =>6)
+    @activities = Category.find(:all, :conditions => {:private => false, :type => "Activity", :parent_id => params[:parent]}, :limit =>6)
   end
   
   def new
@@ -12,7 +12,11 @@ class ActivitiesController < ApplicationController
   end
   
   def create
-    activity = Activity.find_or_create(params[:activity])
+    activity = Activity.find_or_innitialize(params[:activity])
+    if activity.new_record?
+      activity.private = true
+      activity.save!
+    end
     redirect_to new_time_span_url(activity)
   end
   
