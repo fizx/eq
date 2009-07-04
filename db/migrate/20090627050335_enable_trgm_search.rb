6f937e8e60b76168bb17fc9e2c4c0c223938e9eb
Kyle Maxwell <kyle@kylemaxwell.com>
2009-07-03 20:11:38 -0700

    WIP

--- db/migrate/20090627050335_enable_trgm_search.rb
+++ db/migrate/20090627050335_enable_trgm_search.rb
@@ -1,6 +1,6 @@
 class EnableTrgmSearch < ActiveRecord::Migration
   def self.up
-    cmd = "find /opt/local/ -name pg_trgm.sql | xargs cat | psql #{ActiveRecord::Base.connection.current_database}"
+    cmd = "find / -name pg_trgm.sql | xargs cat | psql #{ActiveRecord::Base.connection.current_database}"
     puts cmd
     system cmd
   end
--- db/migrate/20090630220932_enable_earthdistance.rb
+++ db/migrate/20090630220932_enable_earthdistance.rb
@@ -1,6 +1,6 @@
 class EnableEarthdistance < ActiveRecord::Migration
   def self.up
-    cmd = "find /opt/local/ -name earthdistance.sql | xargs cat | psql #{ActiveRecord::Base.connection.current_database}"
+    cmd = "find / -name earthdistance.sql | xargs cat | psql #{ActiveRecord::Base.connection.current_database}"
     puts cmd
     system cmd
   end
