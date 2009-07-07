21122298d0ee99fe9e7913a03909a7f1dc13ddbc
Kyle Maxwell <kyle@kylemaxwell.com>
2009-07-07 13:42:41 -0700

    date ranges

--- lib/date_range.rb
+++ lib/date_range.rb
@@ -12,30 +12,6 @@ class DateRange
     b = Chronic.parse(b, :guess => false, :now => a).last
     new a..b
   end
-
-
-  # describe "#to_s" do
-  #   it "should handle same day" do
-  #     DateRange.parse("jan 1 2009 8am-5pm").to_s.should == "Jan 1 8am-5pm"
-  #     DateRange.parse("jan 1 2009 8am-10am").to_s.should == "Jan 1 8-10am"
-  #     DateRange.parse("jan 1 2015 8am-10am").to_s.should == "Jan 1 2015 8-10am"
-  #   end
-  #   
-  #   it "should handle cross days" do
-  #     DateRange.parse("jan 1 2015 8am - jan 2 10am").to_s.should == "Jan 1 2015 8am - Jan 2 2015 10am"
-  #     DateRange.parse("jan 1 8am - jan 2 10am").to_s.should == "Jan 1 8am - Jan 2 10am"
-  #     DateRange.parse("dec 25 2009 8am - jan 2 2010 10am").to_s.should == "Dec 25 8am - Jan 2 10am"
-  #   end
-  #   
-  #   it "should handle minutes" do
-  #     DateRange.parse("aug 25 8pm - 8:30pm").to_s.should == "Aug 25 8-8:30pm"
-  #   end
-  #   
-  #   it "should handle cross months" do
-  #     DateRange.parse("aug 25 - sept 3").to_s.should == "Aug 25 - Sep 3"
-  #   end
-  # end
-  # 
   
   def to_s
     m1 = first.min == 0 ? "" : ":%1M"
